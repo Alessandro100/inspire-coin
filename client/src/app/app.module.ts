523e8c440623c70451179d26acdd5bb525e70f9d
arthur-del <arthur.tourneyrie@gmail.com>
2021-02-26 14:17:52 -0500

    removed bugged component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     HeaderComponent,
     LandingComponent
   ],
