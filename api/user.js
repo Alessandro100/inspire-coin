70dcba64aead65fcd34c06998f26e16f7fedbf3a
Alessandro <kalessandro14@gmail.com>
2021-02-26 20:34:35 -0500

    created a user with password and eth address

--- api/user.js
+++ api/user.js
@@ -1,5 +1,38 @@
+const NodeRSA = require('node-rsa');
+const key = new NodeRSA({b: 512});
 
-
-function createUser() {
-    
+function createUser(db, email, web3) {
+    return new Promise((resolve, reject) => {
+        if(email){
+            db.collection('users').findOne({email}, (err, doc)=>{
+                if(doc) {
+                    reject({"status":"Failed", "reason":"Already registered"})
+                } else {
+                    const newWallet = web3.eth.accounts.create();
+                    const password = generatePassword();
+                    db.collection('users').insertOne({
+                        email: email,
+                        password: key.encrypt(password),
+                        ethAddress: newWallet.address,
+                        privateKey: key.encrypt(newWallet.privateKey, 'base64')
+                    })
+                    resolve({"status": "success", "password": password})
+                }
+            })
+        }else{
+            reject({"status":"Failed", "reason":"wrong input"})
+        }
+    });
 }
+
+function generatePassword() {
+    const length = 8,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+    var password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return password;
+}
+
+module.exports = {createUser}
